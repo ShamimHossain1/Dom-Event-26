3bbb50723e66d316cbbd327da34bf25e78347dbb
ShamimHossain1 <beastb697@gmail.com>
2023-02-11 22:59:25 +0600

    Delegate

---
+++ js/delegate.js
@@ -0,0 +1,23 @@
+// const items = document.getElementsByClassName('item');
+// for (const item of items) {
+//     item.addEventListener('click', function (event) {
+//         event.target.parentNode.removeChild(event.target);
+//     })
+// }
+
+document.getElementById('list-container').addEventListener('click', function (event) {
+    // console. log(event. target);
+    event.target.parentNode.removeChild(event.target)
+})
+
+
+
+document.getElementById('btn-add-item').addEventListener('click', function () {
+    const listContainer = document.getElementById('list-container');
+
+    const li = document.createElement('li');
+    li.innerText = 'Brand New Item added';
+    // li.classList.add('item');
+    listContainer.appendChild(li);
+
+})
